d69e5804e9843bc6c2dded8f2185674a8edd8b78
Arun jR <arun@Aruns-MacBook-Pro.local>
2023-12-16 20:04:55 +0545

    Website Caoursel update

---
+++ src/App.js
@@ -0,0 +1,32 @@
+import React from "react";
+import "./App.css";
+import Navbar from "./components/Navbar";
+// import { BrowserRouter, } from "react-router-dom";
+import {Routes, Route, } from 'react-router-dom'
+import Footer from "./components/pages/Footer";
+import Home  from "./components/pages/Home";
+import Services from "./components/pages/Services";
+import About from "./components/pages/About";
+import Contact from "./components/pages/Contact";
+import Carousel from "./components/carousel/Carousel";
+
+
+function App() {
+  return (
+    <>
+    
+    <Footer/>
+    <Navbar/>
+    <Carousel/>
+      <Routes>  
+        <Route path='/home' element={<Home/>}  ></Route>
+        <Route path='/services' element={<Services/>}  ></Route>
+        <Route path='/about' element={<About/>}  ></Route>
+        <Route path='/contact' element={<Contact/>}  ></Route>
+      </Routes>
+
+    </>
+  );
+}
+
+export default App;
